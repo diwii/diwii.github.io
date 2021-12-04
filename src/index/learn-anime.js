e55185eacde02e7735f02f5de09a52df0899d866
Valdis Locs <valdis.locs@gmail.com>
2021-12-05 01:03:26 +0200

    Add styles, Add circle and photo-frame

--- src/index/learn-anime.js
+++ src/index/learn-anime.js
@@ -1,11 +1,36 @@
 var anime = require('animejs');
 console.log('HELLO1');
 
-anime({
+let squareAnimation = anime({
     targets: ".square",
     translateX: 250,
     rotateZ: 360,
     scale: 3,
     duration: 3000,
     loop: true
-})
+});
+
+let circleAnimation = anime({
+    targets: ".circle",
+    keyframes: [
+        {translateY: 250},
+        {translateX: 250},
+        {translateY: 50},
+        {translateX: 50},
+    ],
+    scale: .5,
+    duration: 5000,
+    loop: true,
+    direction: "alternate"
+});
+
+let photoFrame = anime({
+    targets: ".photo-frame",
+    keyframes: [
+        {    translateX: '50%', translateY: '100vh'},
+        {     translateX: '50%', translateY: '0', delay: '3000'}
+    ],
+    duration: 3000,
+    loop: true,
+    // direction: "alternate"
+});
